47adfbebb387493b47956d2833918fe8d1aebc5f
= <=>
2019-12-08 11:10:55 -0300

    preparando para testar no linux

--- src/client.py
+++ src/client.py
@@ -5,15 +5,25 @@ port_proxy = 3128
 ip_client = '192.168.1.3'
 port_client = 3129
 ip_server = '192.168.1.3'
+port_server = 3130
 
 addr_proxy = (ip_proxy, port_proxy) 
+addr_client = (ip_client, port_client)
+addr_server = (ip_server, port_server)
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
 client_socket.connect(addr_proxy)
-
-print("Para sair digite e mande'sair'\n")
+print("Digite a mensagem para ser enviada para o server\n")
 msg = input()
-while msg != 'sair':
-    client_socket.send(bytes(msg,'utf-8'))
-    msg = input()
+client_socket.send(bytes(msg,'utf-8'))
 client_socket.close()
+
+client2_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+client2_socket.bind(addr_client) 
+client2_socket.listen(5)
+print('aguardando resposta do server...')
+con, server = client2_socket.accept()
+resposta = con.recv(1024).decode('utf-8')
+print(resposta)
+client2_socket.close()
--- src/proxy.py
+++ src/proxy.py
@@ -5,19 +5,27 @@ port_proxy = 3128
 ip_client = '192.168.1.3'
 port_client = 3129
 ip_server = '192.168.1.3'
+port_server = 3130
 
 addr_proxy = (ip_proxy, port_proxy) 
+addr_client = (ip_client, port_client)
+addr_server = (ip_server, port_server)
+
 proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 proxy_socket.bind(addr_proxy) 
-proxy_socket.listen(1)
+proxy_socket.listen(5)
 
 while True:
     print('aguardando conexao...')
     con, client = proxy_socket.accept()
     print('concectado com '+client[0])
-    while True:
-        msg = con.recv(1024).decode('utf-8')
-        if not msg: break
-        print(client, msg)       
+    msg = con.recv(1024).decode('utf-8')
+    if not msg: proxy_socket.close()
+    print(client, msg)       
     print('finalizando conexao do cliente '+client[0])
-    con.close()
+    proxy_socket.close()
+
+    proxy2_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    proxy2_socket.connect(addr_server)
+    proxy2_socket.send(bytes(msg,'utf-8'))
+    proxy2_socket.close()
